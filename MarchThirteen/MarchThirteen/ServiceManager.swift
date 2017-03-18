1c347a300e85d7b2d797f3a0132817cec5b8346b
Luis Reisewitz <reisewitz@me.com>
2017-03-18 16:43:14 +0100

    cleanup servicemanager

--- MarchThirteen/MarchThirteen/ServiceManager.swift
+++ MarchThirteen/MarchThirteen/ServiceManager.swift
@@ -8,151 +8,164 @@
 
 import Foundation
 import MultipeerConnectivity
-import RxSwift
-
-// Service type must be a unique string, at most 15 characters long
-// and can contain only ASCII lowercase letters, numbers and hyphens.
-private let chatRoomServiceType = "marchthirteen"
-private let myPeerId = MCPeerID(displayName: UIDevice.current.name)
-
-struct NetworkMessage {
-    let text: String
-}
-
-enum NetworkEvent {
-    case message(message: NetworkMessage)
-}
 
 class ServiceManager: NSObject {
     // MARK: Public Shit
-    let peers = Variable<[MCPeerID]>([myPeerId])
     
-    // MARK: Private Shit
-    private let serviceAdvertiser: MCNearbyServiceAdvertiser
-    private let serviceBrowser: MCNearbyServiceBrowser
+    typealias PeersChangedCallback = (_ peers: [MCPeerID]) -> Void
+    typealias DataReceivedCallback = (_ data: Data, _ peer: MCPeerID) -> Void
     
-    lazy var session : MCSession = {
-        let session = MCSession(peer: myPeerId, securityIdentity: nil, encryptionPreference: .none)
+    /// The list of currently connected peers (peers in state MCSessionState.connected).
+    var connectedPeers: [MCPeerID] {
+        return session.connectedPeers
+    }
+
+    /// The peer id of the local user.
+    fileprivate let ownPeer: MCPeerID
+
+    /// This callback will be called when the list of connected peers changes.
+    var peersChanged: PeersChangedCallback?
+    /// This callback will be called when the session receives data from one of
+    /// the connected peers.
+    var dataReceived: DataReceivedCallback?
+    
+    // MARK: Private MC Session Stuff
+
+    /// The service type that is used for advertising and browsing.
+    fileprivate let serviceType: String
+    
+    /// Session used for communicating with peers.
+    fileprivate lazy var session: MCSession = {
+        let session = MCSession(
+            peer: self.ownPeer, securityIdentity: nil, encryptionPreference: .none)
         session.delegate = self
         return session
     }()
     
-    override init() {
-        self.serviceAdvertiser = MCNearbyServiceAdvertiser(peer: myPeerId, discoveryInfo: nil, serviceType: chatRoomServiceType)
-        self.serviceBrowser = MCNearbyServiceBrowser(peer: myPeerId, serviceType: chatRoomServiceType)
-        
+    /// Advertises our peer to others.
+    fileprivate lazy var serviceAdvertiser: MCNearbyServiceAdvertiser = {
+        let advertiser = MCNearbyServiceAdvertiser(
+            peer: self.ownPeer, discoveryInfo: nil, serviceType: self.serviceType)
+        advertiser.delegate = self
+        return advertiser
+    }()
+    
+    /// Browses for other peers.
+    fileprivate lazy var serviceBrowser: MCNearbyServiceBrowser = {
+        let browser = MCNearbyServiceBrowser(
+            peer: self.ownPeer, serviceType: self.serviceType)
+        browser.delegate = self
+        return browser
+    }()
+    
+    /// Initializes the service manager with a given service type and peerID.
+    ///
+    /// - Parameters:
+    ///   - serviceType: A MultiPeer service type. Service type must be a unique 
+    ///     string, at most 15 characters long and can contain only ASCII lowercase 
+    ///     letters, numbers and hyphens.
+    ///   - peerID: The user's own peer id to be shown to others.
+    init(for serviceType: String, as peerID: MCPeerID) {
+        self.serviceType = serviceType
+        self.ownPeer = peerID
         super.init()
-        
-        self.serviceAdvertiser.delegate = self
-        self.serviceAdvertiser.startAdvertisingPeer()
-        
-        self.serviceBrowser.delegate = self
-        self.serviceBrowser.startBrowsingForPeers()
     }
-    
-    func send(event: NetworkEvent) {
-        guard session.connectedPeers.count > 0 else { return }
-        var copy = event
-        let data = Data(bytes: &copy, count: MemoryLayout<NetworkEvent>.size(ofValue: event))
-        try! self.session.send(data, toPeers: session.connectedPeers, with: .reliable)
-    }
-    
-    func receive(data: Data) {
-        let pointer = UnsafeMutableBufferPointer<NetworkEvent>. .alloc(capacity: MemoryLayout<NetworkEvent>.size)
-        data.copyBytes(to: pointer)
-        
-    }
-    
-    
-    
-//    func send(counter: Int) {
-//        //print("%@", "sendString: \(counter) to \(session.connectedPeers.count) peers")
-//        
-//        guard session.connectedPeers.count > 0 else { return }
-//        do {
-//            let data = Data(bytes: [UInt8(counter)])
-//            try self.session.send(data, toPeers: session.connectedPeers, with: .reliable)
-//        }
-//        catch let error {
-//            //print("%@", "Error for sending: \(error)")
-//        }
-//        
-//    }
     
     deinit {
-        self.serviceAdvertiser.stopAdvertisingPeer()
-        self.serviceBrowser.stopBrowsingForPeers()
+        // Stop MultiPeer Stuff
+        stopService()
     }
     
 }
 
+// MARK: - Public API
+extension ServiceManager {
+    /// Starts advertising & browsing for our service.
+    func startService() {
+        serviceAdvertiser.startAdvertisingPeer()
+        serviceBrowser.startBrowsingForPeers()
+    }
+    
+    /// Stops advertising & browsing for our service.
+    func stopService() {
+        serviceAdvertiser.stopAdvertisingPeer()
+        serviceBrowser.stopBrowsingForPeers()
+    }
+    
+    /// Tries to send given data to all connected peers.
+    ///
+    /// - Parameter data: data to be sent to peers.
+    func send(data: Data) {
+        guard session.connectedPeers.count > 0 else { return }
+        do {
+            try session.send(
+                data, toPeers: session.connectedPeers, with: .reliable)
+        } catch {
+            print("Sending of data failed with error \(error)")
+        }
+    }
+}
+
+// MARK: - Private Delegate Processing
+fileprivate extension ServiceManager {
+    func notifyReceive(data: Data, from peer: MCPeerID) {
+        dataReceived?(data, peer)
+    }
+    
+    func notifyPeersChanged(peers: [MCPeerID]) {
+        peersChanged?(peers)
+    }
+}
+
+// MARK: - MCNearbyServiceAdvertiserDelegate
 extension ServiceManager: MCNearbyServiceAdvertiserDelegate {
+    func advertiser(
+        _ advertiser: MCNearbyServiceAdvertiser,
+        didReceiveInvitationFromPeer peerID: MCPeerID,
+        withContext context: Data?,
+        invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+     
+        // Automatically accept all invitations that we get
+        invitationHandler(true, session)
+    }
+    
+    // NOOPs
     
     func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
-        //print("%@", "didNotStartAdvertisingPeer: \(error)")
+        // TODO: Handle advertising start errors
     }
-    
-    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
-        //print("%@", "didReceiveInvitationFromPeer \(peerID)")
-        invitationHandler(true, self.session)
-    }
-    
 }
 
+// MARK: - MCNearbyServiceBrowserDelegate
 extension ServiceManager: MCNearbyServiceBrowserDelegate {
-    
-    func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
-        //print("%@", "didNotStartBrowsingForPeers: \(error)")
-    }
-    
     func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
-        //print("%@", "foundPeer: \(peerID)")
-        //print("%@", "invitePeer: \(peerID)")
+        // Automatically invite all peers we find
         browser.invitePeer(peerID, to: session, withContext: nil, timeout: 10)
     }
     
-    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
-        //print("%@", "lostPeer: \(peerID)")
-    }
+    // NOOPs
     
+    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+        // TODO: handle pending / invitable peers
+    }
+    func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
+        // TODO: Handle browsing start errors
+    }
 }
 
+// MARK: - MCSessionDelegate
 extension ServiceManager: MCSessionDelegate {
-    
     func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
-        switch state {
-        case .connected:
-            var copy = peers.value
-            guard copy.index(of: peerID) == nil else { return }
-            copy.append(peerID)
-            peers.value = copy
-        case .notConnected:
-            var copy = peers.value
-            guard let index = copy.index(of: peerID) else { return }
-            copy.remove(at: index)
-            peers.value = copy
-        default:
-            break
-        }
+        notifyPeersChanged(peers: connectedPeers)
     }
     
     func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
-//        let counter = Int(data[0])
-        //print("%@", "didReceiveCount: \(counter)")
-        
-        // FIXME: create messages
+        notifyReceive(data: data, from: peerID)
     }
     
-    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
-        //print("%@", "didReceiveStream")
-    }
+    // NOOPs
     
-    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
-        //print("%@", "didStartReceivingResourceWithName")
-    }
-    
-    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL, withError error: Error?) {
-        //print("%@", "didFinishReceivingResourceWithName")
-    }
-    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {}
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {}
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL, withError error: Error?) {}
 }
